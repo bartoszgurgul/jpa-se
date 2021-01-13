65b5fa998b81b9d747ce824a081a1b40c6128061
bartoszgurgul <bartoszgurgul@gmail.com>
2021-01-13 23:33:32 +0100

    final

--- src/main/java/pl/javastart/dao/BookDao.java
+++ src/main/java/pl/javastart/dao/BookDao.java
@@ -1,4 +1,9 @@
 package pl.javastart.dao;
 
-public class BookDao {
+import pl.javastart.model.Book;
+
+public interface BookDao {
+    void save(Book book);
+    Book get(Long id);
+    void cleanUp();
 }
